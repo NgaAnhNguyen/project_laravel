cc2afcab7697b09cbfa30ad46a29d9f11fd15fd6
Anh Nga <nguyenanhnga11@gmail.com>
2024-12-03 14:37:34 +0700

    Fix 3.12.2024

--- project-laravel/resources/views/layout.blade.php
+++ project-laravel/resources/views/layout.blade.php
@@ -56,7 +56,7 @@
 					<header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
         <div class="flex lg:justify-center lg:col-start-2">
         </div>
-        @if (Route::has('login'))
+        
         <nav class="-mx-3 flex flex-1 justify-end">
             @auth
             <a
@@ -89,7 +89,7 @@
             @endif
             @endauth
         </nav>
-        @endif
+      
     </header>
 
 
